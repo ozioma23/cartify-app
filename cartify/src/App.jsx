279c238163996caf687e88b5f173ac743b7d6c96
OgborFaith <faithogbor23@gmail.com>
2025-03-26 18:05:03 +0100

    loginpage added and its a work in progress

--- cartify/src/App.jsx
+++ cartify/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserContextProvider } from "./context/UserContext";  
 import LandingPage from "./pages/LandingPage"
+import LoginPage from "./pages/LoginPage";
 
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
       <Router>  
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
         </Routes>
       </Router>
     </UserContextProvider>   
--- cartify/src/pages/LoginPage.jsx
+++ cartify/src/pages/LoginPage.jsx
@@ -1,1 +1,38 @@
+import { useState, useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const { loginUser } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+
+    return (
+        <>
+            <div>
+                <h1>Login to your Cartify account</h1>
+                <form>
+                    <div>
+                        <input type="email" value={email}></input>
+                    </div>
+                    <div>
+                        <input type="password" value={password}></input>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+
+                <button type="submit">Continue as guest</button>
+                <p className="pb-10 mt-16 md:mt-24 text-lg md:tet-xl lg:text-2xl">
+                    Don't have an account?
+                    <span  className="text-secondary cursor-pointer"> Sign Up</span>
+                </p>
+            </div>
+
+        </>
+    )
+}
 export default LoginPage
