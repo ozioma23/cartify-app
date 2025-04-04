c1aa09dc437cdd32a91990ddb8f816a2b094ebc6
OgborFaith <faithogbor23@gmail.com>
2025-04-04 05:18:56 +0100

    more changes made

--- cartify/src/pages/LandingPage.jsx
+++ cartify/src/pages/LandingPage.jsx
@@ -25,7 +25,7 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="overflow-auto lg:overflow-hidden sm:overflow-visible w-full flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen px-8 md:px-12 lg:px-24 bg-primary text-white">
+      <div className=" w-full flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen px-8 md:px-12 lg:px-24 bg-primary text-white">
         <div className="flex flex-col text-left lg:text-left lg:p1-12 w-full lg:w-1/2">
           <h1 className="text-2xl md:text-5xl lg:text-6xl font-bold fade-in-text">
             Welcome to
@@ -75,17 +75,6 @@ const LandingPage = () => {
           />
         </div>
       </div>
-      <img
-        src="/shapes/irregular1.svg"
-        alt="Irregular Shape 1"
-        className="absolute top-[-30px] lg:top-[20px] left-0 sm:left-[-15px] w-[300px] h-[250px] sm:w-[400px] sm:h-[350px] md:w-[500px] md:h-[450px] lg:w-[600px] lg:h-[500px] pointer-events-none"
-      />
-
-      <img
-        src="/shapes/irregular2.svg"
-        alt="Irregular Shape 2"
-        className="absolute bottom-[-50px] md:bottom-[-20px] right-0 w-[300px] h-[250px] sm:w-[350px] sm:h-[300px] md:w-[400px] md:h-[350px] lg:w-[450px] lg:h-[400px] pointer-events-none"
-      />
     </>
   );
 };
